d41a71ac3a1388916d9014d22b6b915a1aa542e2
Shreyash Prashu <shreyash.prashu@nmbrsystems.com>
2017-11-25 05:55:22 +0530

    Swagger issue fixed

--- src/main/java/com/csquare/ums/CodeXApplication.java
+++ src/main/java/com/csquare/ums/CodeXApplication.java
@@ -1,34 +1,39 @@
 package com.csquare.ums;
 
-import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-import com.csquare.ums.config.AppInitializer;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 public class CodeXApplication extends SpringBootServletInitializer {
 
+   // public static void main(String[] args) {
+
+       // System.setProperty("CONTAINER", "SELF");
+        //SpringApplicationBuilder application = new SpringApplicationBuilder();
+        //application.logStartupInfo(false);
+       // application.bannerMode(Banner.Mode.OFF);
+       // application.sources(CodeXApplication.class, AppInitializer.class);
+
+       // application.run(args);
+
+  //  }
     public static void main(String[] args) {
+		SpringApplication.run(CodeXApplication.class, args);
+	}
 
-        System.setProperty("CONTAINER", "SELF");
-        SpringApplicationBuilder application = new SpringApplicationBuilder();
-        application.logStartupInfo(false);
-        application.bannerMode(Banner.Mode.OFF);
-        application.sources(CodeXApplication.class, AppInitializer.class);
-
-        application.run(args);
-
-    }
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CodeXApplication.class);
+	}
 
     /**
      * Server hook. will be used when deployed as war
      */
-    @Override
+   /* @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 
         System.setProperty("CONTAINER", "SERVER");
@@ -37,5 +42,5 @@ public class CodeXApplication extends SpringBootServletInitializer {
         application.sources(CodeXApplication.class, AppInitializer.class);
 
         return application;
-    }
+    }*/
 }
--- src/main/java/com/csquare/ums/config/WebAppConfig.java
+++ src/main/java/com/csquare/ums/config/WebAppConfig.java
@@ -24,6 +24,8 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
     }
 
     @Override
