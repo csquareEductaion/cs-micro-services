a393f397fe978b5c97a81e857000fe2c30ed6d4e
Rahul Singh <rahul.singh@nmbrsystems.com>
2018-01-23 23:10:30 +0530

    Changed Variable name according to UI in studentMGT

--- cs_student_mgt/src/main/java/com/csquare/student/model/Student.java
+++ cs_student_mgt/src/main/java/com/csquare/student/model/Student.java
@@ -1,5 +1,6 @@
 package com.csquare.student.model;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -47,87 +48,88 @@ public class Student {
     private String gender;
     
     @Column(name = "parent_name")
-    private String parent_name;
+    private String parentName;
     
     @Column(name = "existing_client")
-    private String existing_client;
+    private String existingClient;
     
     @Column(name = "source_of_lead")
-    private String source_of_lead;
+    private String sourceOfLead;
     
     @Column(name = "is_hold")
-    private String is_hold;
+    private String isHold;
     
     @Column(name = "student_record_owner")
-    private String student_record_owner;
+    private String studentRecordOwner;
     
     @Column(name = "is_parent_doc_submitted")
-    private String is_parent_doc_submitted;
+    private String parentsDocStatus;
+    
+    @Column(name = "is_tutor_doc_submitted")
+    private String tutorDocStatus;
     
     @Column(name = "is_intrested")
-    private String is_intrested;
+    private String interested;
     
     @Column(name = "is_not_interested")
-    private String is_not_interested;
+    private String notInterested;
     
     @Column(name = "is_demo_required")
-    private String is_demo_required;
+    private String demoRequired;
     
     @Column(name = "demo_date")
-    private String demo_date;
+    private String demoDate;
     
     @Column(name = "is_converted")
-    private String is_converted;
+    private String converted;
     
     @Column(name = "enrollment_date")
-    private String enrollment_date;
+    private String enrollmentDate;
     
     @Column(name = "is_followup_required")
-    private String is_followup_required;
+    private String requiredFollowUp;
     
     @Column(name = "next_followup_date")
-    private String next_followup_date;
+    private String followUpDate;
     
     @Column(name = "preffered_call_time")
-    private String preffered_call_time;
+    private String callPreferredTime;
     
     @Column(name = "is_conversion_pending")
-    private String is_conversion_pending;
+    private String pendingForConversion;
     
     @Column(name = "is_home_tuition_stop")
-    private String is_home_tuition_stop;
+    private String isStopped;
     
     @Column(name = "class_stop_date")
-    private String class_stop_date;
-    
-    @Column(name = "is_class_stop")
-    private String is_class_stop;
-    
+    private String stoppedDate;
+        
     @Column(name = "is_agreement_sent_to_parent")
-    private String is_agreement_sent_to_parent;
+    private String parentAgreementStatus;
     
     @Column(name = "is_agreemenet_sent_to_tutor")
-    private String is_agreemenet_sent_to_tutor;
+    private String tutorAgreementStatus;
     
     @Column(name = "package_in_hrs")
-    private String package_in_hrs;
+    private String packageInHrs;
     
     @Column(name = "hourly_rate")
-    private String hourly_rate;
+    private String hourlyRate;
     
     @Column(name = "registration_fee_amount")
-    private String registration_fee_amount;
+    private String registrationFee;
     
     @Column(name = "is_registration_paid")
-    private String is_registration_paid;
+    private String isRegistrationPaid;
     
     @Column(name = "total_tuition_fee")
-    private String total_tuition_fee;
+    private String studentTotalFee;
     
     @Column(name = "csquare_income")
-    private String csquare_income;
+    private String csquareIncome;
     
-   
+   @Column(name = "tuition_required_for_months")
+   private String tutionRequiredForMonths;
 
 	public String getpK() {
 		return pK;
@@ -201,222 +203,231 @@ public class Student {
 		this.gender = gender;
 	}
 
-	public String getParent_name() {
-		return parent_name;
+	public String getParentName() {
+		return parentName;
 	}
 
-	public void setParent_name(String parent_name) {
-		this.parent_name = parent_name;
+	public void setParentName(String parentName) {
+		this.parentName = parentName;
 	}
 
-	public String getExisting_client() {
-		return existing_client;
+	public String getExistingClient() {
+		return existingClient;
 	}
 
-	public void setExisting_client(String existing_client) {
-		this.existing_client = existing_client;
+	public void setExistingClient(String existingClient) {
+		this.existingClient = existingClient;
 	}
 
-	public String getSource_of_lead() {
-		return source_of_lead;
+	public String getSourceOfLead() {
+		return sourceOfLead;
 	}
 
-	public void setSource_of_lead(String source_of_lead) {
-		this.source_of_lead = source_of_lead;
+	public void setSourceOfLead(String sourceOfLead) {
+		this.sourceOfLead = sourceOfLead;
 	}
 
-	public String getIs_hold() {
-		return is_hold;
+	public String getIsHold() {
+		return isHold;
 	}
 
-	public void setIs_hold(String is_hold) {
-		this.is_hold = is_hold;
+	public void setIsHold(String isHold) {
+		this.isHold = isHold;
 	}
 
-	public String getStudent_record_owner() {
-		return student_record_owner;
+	public String getStudentRecordOwner() {
+		return studentRecordOwner;
 	}
 
-	public void setStudent_record_owner(String student_record_owner) {
-		this.student_record_owner = student_record_owner;
+	public void setStudentRecordOwner(String studentRecordOwner) {
+		this.studentRecordOwner = studentRecordOwner;
 	}
 
-	public String getIs_parent_doc_submitted() {
-		return is_parent_doc_submitted;
+	public String getParentsDocStatus() {
+		return parentsDocStatus;
 	}
 
-	public void setIs_parent_doc_submitted(String is_parent_doc_submitted) {
-		this.is_parent_doc_submitted = is_parent_doc_submitted;
+	public void setParentsDocStatus(String parentsDocStatus) {
+		this.parentsDocStatus = parentsDocStatus;
 	}
 
-	public String getIs_intrested() {
-		return is_intrested;
+	public String getTutorDocStatus() {
+		return tutorDocStatus;
 	}
 
-	public void setIs_intrested(String is_intrested) {
-		this.is_intrested = is_intrested;
+	public void setTutorDocStatus(String tutorDocStatus) {
+		this.tutorDocStatus = tutorDocStatus;
 	}
 
-	public String getIs_not_interested() {
-		return is_not_interested;
+	public String getInterested() {
+		return interested;
 	}
 
-	public void setIs_not_interested(String is_not_interested) {
-		this.is_not_interested = is_not_interested;
+	public void setInterested(String interested) {
+		this.interested = interested;
 	}
 
-	public String getIs_demo_required() {
-		return is_demo_required;
+	public String getNotInterested() {
+		return notInterested;
 	}
 
-	public void setIs_demo_required(String is_demo_required) {
-		this.is_demo_required = is_demo_required;
+	public void setNotInterested(String notInterested) {
+		this.notInterested = notInterested;
 	}
 
-	public String getDemo_date() {
-		return demo_date;
+	public String getDemoRequired() {
+		return demoRequired;
 	}
 
-	public void setDemo_date(String demo_date) {
-		this.demo_date = demo_date;
+	public void setDemoRequired(String demoRequired) {
+		this.demoRequired = demoRequired;
 	}
 
-	public String getIs_converted() {
-		return is_converted;
+	public String getDemoDate() {
+		return demoDate;
 	}
 
-	public void setIs_converted(String is_converted) {
-		this.is_converted = is_converted;
+	public void setDemoDate(String demoDate) {
+		this.demoDate = demoDate;
 	}
 
-	public String getEnrollment_date() {
-		return enrollment_date;
+	public String getConverted() {
+		return converted;
 	}
 
-	public void setEnrollment_date(String enrollment_date) {
-		this.enrollment_date = enrollment_date;
+	public void setConverted(String converted) {
+		this.converted = converted;
 	}
 
-	public String getIs_followup_required() {
-		return is_followup_required;
+	public String getEnrollmentDate() {
+		return enrollmentDate;
 	}
 
-	public void setIs_followup_required(String is_followup_required) {
-		this.is_followup_required = is_followup_required;
+	public void setEnrollmentDate(String enrollmentDate) {
+		this.enrollmentDate = enrollmentDate;
 	}
 
-	public String getNext_followup_date() {
-		return next_followup_date;
+	public String getRequiredFollowUp() {
+		return requiredFollowUp;
 	}
 
-	public void setNext_followup_date(String next_followup_date) {
-		this.next_followup_date = next_followup_date;
+	public void setRequiredFollowUp(String requiredFollowUp) {
+		this.requiredFollowUp = requiredFollowUp;
 	}
 
-	public String getPreffered_call_time() {
-		return preffered_call_time;
+	public String getFollowUpDate() {
+		return followUpDate;
 	}
 
-	public void setPreffered_call_time(String preffered_call_time) {
-		this.preffered_call_time = preffered_call_time;
+	public void setFollowUpDate(String followUpDate) {
+		this.followUpDate = followUpDate;
 	}
 
-	public String getIs_conversion_pending() {
-		return is_conversion_pending;
+	public String getCallPreferredTime() {
+		return callPreferredTime;
 	}
 
-	public void setIs_conversion_pending(String is_conversion_pending) {
-		this.is_conversion_pending = is_conversion_pending;
+	public void setCallPreferredTime(String callPreferredTime) {
+		this.callPreferredTime = callPreferredTime;
 	}
 
-	public String getIs_home_tuition_stop() {
-		return is_home_tuition_stop;
+	public String getPendingForConversion() {
+		return pendingForConversion;
 	}
 
-	public void setIs_home_tuition_stop(String is_home_tuition_stop) {
-		this.is_home_tuition_stop = is_home_tuition_stop;
+	public void setPendingForConversion(String pendingForConversion) {
+		this.pendingForConversion = pendingForConversion;
 	}
 
-	public String getClass_stop_date() {
-		return class_stop_date;
+	public String getIsStopped() {
+		return isStopped;
 	}
 
-	public void setClass_stop_date(String class_stop_date) {
-		this.class_stop_date = class_stop_date;
+	public void setIsStopped(String isStopped) {
+		this.isStopped = isStopped;
 	}
 
-	public String getIs_class_stop() {
-		return is_class_stop;
+	public String getStoppedDate() {
+		return stoppedDate;
 	}
 
-	public void setIs_class_stop(String is_class_stop) {
-		this.is_class_stop = is_class_stop;
+	public void setStoppedDate(String stoppedDate) {
+		this.stoppedDate = stoppedDate;
 	}
 
-	public String getIs_agreement_sent_to_parent() {
-		return is_agreement_sent_to_parent;
+	public String getParentAgreementStatus() {
+		return parentAgreementStatus;
 	}
 
-	public void setIs_agreement_sent_to_parent(String is_agreement_sent_to_parent) {
-		this.is_agreement_sent_to_parent = is_agreement_sent_to_parent;
+	public void setParentAgreementStatus(String parentAgreementStatus) {
+		this.parentAgreementStatus = parentAgreementStatus;
 	}
 
-	public String getIs_agreemenet_sent_to_tutor() {
-		return is_agreemenet_sent_to_tutor;
+	public String getTutorAgreementStatus() {
+		return tutorAgreementStatus;
 	}
 
-	public void setIs_agreemenet_sent_to_tutor(String is_agreemenet_sent_to_tutor) {
-		this.is_agreemenet_sent_to_tutor = is_agreemenet_sent_to_tutor;
+	public void setTutorAgreementStatus(String tutorAgreementStatus) {
+		this.tutorAgreementStatus = tutorAgreementStatus;
 	}
 
-	public String getPackage_in_hrs() {
-		return package_in_hrs;
+	public String getPackageInHrs() {
+		return packageInHrs;
 	}
 
-	public void setPackage_in_hrs(String package_in_hrs) {
-		this.package_in_hrs = package_in_hrs;
+	public void setPackageInHrs(String packageInHrs) {
+		this.packageInHrs = packageInHrs;
 	}
 
-	public String getHourly_rate() {
-		return hourly_rate;
+	public String getHourlyRate() {
+		return hourlyRate;
 	}
 
-	public void setHourly_rate(String hourly_rate) {
-		this.hourly_rate = hourly_rate;
+	public void setHourlyRate(String hourlyRate) {
+		this.hourlyRate = hourlyRate;
 	}
 
-	public String getRegistration_fee_amount() {
-		return registration_fee_amount;
+	public String getRegistrationFee() {
+		return registrationFee;
 	}
 
-	public void setRegistration_fee_amount(String registration_fee_amount) {
-		this.registration_fee_amount = registration_fee_amount;
+	public void setRegistrationFee(String registrationFee) {
+		this.registrationFee = registrationFee;
 	}
 
-	public String getIs_registration_paid() {
-		return is_registration_paid;
+	public String getIsRegistrationPaid() {
+		return isRegistrationPaid;
 	}
 
-	public void setIs_registration_paid(String is_registration_paid) {
-		this.is_registration_paid = is_registration_paid;
+	public void setIsRegistrationPaid(String isRegistrationPaid) {
+		this.isRegistrationPaid = isRegistrationPaid;
 	}
 
-	public String getTotal_tuition_fee() {
-		return total_tuition_fee;
+	public String getStudentTotalFee() {
+		return studentTotalFee;
 	}
 
-	public void setTotal_tuition_fee(String total_tuition_fee) {
-		this.total_tuition_fee = total_tuition_fee;
+	public void setStudentTotalFee(String studentTotalFee) {
+		this.studentTotalFee = studentTotalFee;
 	}
 
-	public String getCsquare_income() {
-		return csquare_income;
+	public String getCsquareIncome() {
+		return csquareIncome;
 	}
 
-	public void setCsquare_income(String csquare_income) {
-		this.csquare_income = csquare_income;
+	public void setCsquareIncome(String csquareIncome) {
+		this.csquareIncome = csquareIncome;
 	}
 
+	public String getTutionRequiredForMonths() {
+		return tutionRequiredForMonths;
+	}
+
+	public void setTutionRequiredForMonths(String tutionRequiredForMonths) {
+		this.tutionRequiredForMonths = tutionRequiredForMonths;
+	}
+
+	
 	
 	
 }
